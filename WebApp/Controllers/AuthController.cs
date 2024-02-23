d14abcab3527167c88b8c211cca1ecd255b54ad9
Andreas Fredriksson <afredriksson89@gmail.com>
2024-02-23 09:44:08 +0100

    Add project files.

---
+++ WebApp/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
