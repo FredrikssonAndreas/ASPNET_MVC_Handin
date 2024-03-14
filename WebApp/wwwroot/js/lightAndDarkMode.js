3c3cff9c252a3ccb690ae94b2f08b1b6beccaf35
Andreas Fredriksson <afredriksson89@gmail.com>
2024-03-14 14:36:16 +0100

    Light and dark mode added

--- WebApp/wwwroot/js/lightAndDarkMode.js
+++ WebApp/wwwroot/js/lightAndDarkMode.js
@@ -1,10 +1,14 @@
 var isDarkMode = false;
-
+var logoElement = document.getElementById("logo");
+var logoImage = "/images/silicon-logo.svg";
+logoElement.innerHTML = "<img src='" + logoImage + "' alt='Silicon Logo'>";
+var notFoundElement = document.getElementById("notFoundId");
+var notFoundImage = "/images/404.svg";
+notFoundElement.innerHTML = "<img src='" + notFoundImage + "' alt='404 Not Found'>";
 
 function toggleBothSwitches() {
    
     isDarkMode = !isDarkMode;
-
   
     var desktopSwitch = document.getElementById("switch");
     desktopSwitch.checked = isDarkMode;
@@ -12,13 +16,17 @@ function toggleBothSwitches() {
     
     var mobileSwitch = document.getElementById("switch-mobile");
     mobileSwitch.checked = isDarkMode;
-
    
     if (isDarkMode) {
         activateDarkMode();
+       
     } else {
         deactivateDarkMode();
+        
     }
+
+    updateLogo();
+    update404();
 }
 
 
@@ -29,5 +37,19 @@ function activateDarkMode() {
 function deactivateDarkMode() {
     document.body.classList.remove("dark-mode");
 }
+
+
+function updateLogo() {
+   
+    logoImage = isDarkMode ? "/images/silicon-logo-dark.svg" : "/images/silicon-logo.svg";
+    logoElement.innerHTML = "<img src='" + logoImage + "' alt='Silicon Logo'>";
+}
+
+function update404() {
+
+    notFoundImage = isDarkMode ? "/images/404-dark.svg" : "/images/404.svg";
+    notFoundElement.innerHTML = "<img src='" + notFoundImage + "' alt='404 Not Found'>";
+}
+
 
 
